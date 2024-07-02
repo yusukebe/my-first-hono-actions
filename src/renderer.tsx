7b7ef20584705e2a42ee479e56acf8693f988aea
Yusuke Wada <yusuke@kamawada.com>
2024-07-02 21:50:32 +0900

    remove `global.d.ts`

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,12 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 
 export const renderer = jsxRenderer(
-  ({ children, title }) => {
+  ({ children }) => {
     return (
       <html>
         <head>
           <link href='/static/mvp.css' rel='stylesheet' />
           <link href='/static/style.css' rel='stylesheet' />
-          <title>{title}</title>
         </head>
         <body>
           <header>
